a0249ae4881fec36424361bc9e0c27956542a083
Kieran Lynch <lynch.kieran1@gmail.com>
2020-07-09 12:51:16 -0500

    moved write file function to global and created object for questions

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# README-Generator
+ 
+    # Your Project Title
+
+    kieran project
+
+    ## Description 
+
+    jaosidjf
+
+    ## Table of Contents
+
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+
+
+    ## Installation
+
+    jwier2
+
+
+    ## Usage 
+
+    1242341
+
+    ## Credits
+
+    lksdaur
+
+    ## License
+
+    ISC License
+    ---
+
+    ## Badges
+
+    License MIT
+    ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+    Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+
+    ## Contributing
+
+    alksjdi
+
+    ## Tests
+
+    iwerlkjf
+
+    
--- index.js
+++ index.js
@@ -1,81 +1,98 @@
-// // array of questions for user
-// const questions = [
-// ];
-
-// // function to write README file
-// function writeToFile(fileName, data) {
-// }
-
-// // function to initialize program
-// function init() {
-
-// }
-
-// // function call to initialize program
-// init();
-
-
-
 const inquirer = require("inquirer");
 const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
-const response = inquirer
-  .prompt([
-    {
+// array of questions for user
+const questions = [
+  {
+    type: "input",
+    message: "Enter your project title..",
+    name: "projectName"
+  },
+  {
+    type: "input",
+    message: "Provide a description of your project..",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "Provide instructions on how to install the application..",
+    name: "installation"
+  },
+  {
       type: "input",
-      message: "Enter your project title..",
-      name: "projectName"
-    },
-    {
+      message: "Provide information on how it is used..",
+      name: "usage"
+  },
+  {
       type: "input",
-      message: "Provide a description of your project..",
-      name: "description"
-    },
-    {
+      message: "List any and all credits..",
+      name: "credits"
+  },
+  {
+      type: "list",
+      message: "Select what license your application has..",
+      name: "license",
+      choices: [
+          "MIT",
+          "GNU GPLv3",
+          "Apache License 2.0",
+          "ISC License"
+      ]
+  },
+  {
+      type: "list",
+      message: "Select what badges you would like..",
+      name: "badges",
+      choices: [
+        "Last updated 5 mins ago",
+        "License MIT",
+        "0 GitHub issues",
+      ]
+  },
+  {
       type: "input",
-      message: "Provide instructions on how to install the application..",
-      name: "installation"
-    },
-    {
-        type: "input",
-        message: "Provide information on how it is used..",
-        name: "usage"
-    },
-    {
-        type: "input",
-        message: "List any and all credits..",
-        name: "credits"
-    },
-    {
-        type: "list",
-        message: "Select what license your application has..",
-        name: "license",
-        choices: [
-            "MIT",
-            "GNU GPLv3",
-            "Apache License 2.0",
-            "ISC License"
-        ]
-    },
-    // {
-    //     type: "list",
-    //     message: "Select what badges you would like..",
-    //     name: "badges",
-    //     choices: [
+      message: "List any and all contributors..",
+      name: "contributors"
+  },
+  {
+      type: "input",
+      message: "List any tests that a user can perform..",
+      name: "tests"
+  },
+];
 
-    //     ]
-    // },
-    {
-        type: "input",
-        message: "List any and all contributors..",
-        name: "contributors"
-    },
-    {
-        type: "input",
-        message: "List any tests that a user can perform..",
-        name: "tests"
-    },
-])
+// function to write README file
+function writeToFile(readMeString) {
+  fs.writeFile("./README.md", (readMeString), function (err) {
+    if (err) throw err;
+    console.log('Success!');
+})};
 
-const projectResponse = response.projectName;
-console.log(projectResponse);
+// function to initialize program
+function init() {
+  const response = inquirer
+  .prompt(questions)
+  .then(function(response) { 
+    const data ={}
+    data.title = response.projectName;
+    data.description = response.description;
+    data.installation = response.installation;
+    data.usage = response.usage;
+    data.credits = response.credits;
+    data.license = response.license;
+    data.badges = response.badges;
+    data.contributors = response.contributors;
+    data.tests = response.tests;
+  
+    const readMeString = generateMarkdown(data);
+    writeToFile(readMeString);
+});
+}
+
+// function call to initialize program
+init();
+
+
+
+
