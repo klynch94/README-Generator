2614c058ba99049fed8c0cefcef329cd9830db46
Kieran Lynch <lynch.kieran1@gmail.com>
2020-07-08 19:15:15 -0500

    created file structure and prompt questions

---
+++ index.js
@@ -0,0 +1,77 @@
+// // array of questions for user
+// const questions = [
+// ];
+
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
+
+
+
+var inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Enter your project title..",
+      name: "project name"
+    },
+    {
+      type: "input",
+      message: "Provide a description of your project..",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "Provide instructions on how to install the application..",
+      name: "installation"
+    },
+    {
+        type: "input",
+        message: "Provide information on how it is used..",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "List any and all credits..",
+        name: "credits"
+    },
+    {
+        type: "list",
+        message: "Select what license your application has..",
+        name: "license",
+        choices: [
+            "MIT",
+            "GNU GPLv3",
+            "Apache License 2.0",
+            "ISC License"
+        ]
+    },
+    {
+        type: "list",
+        message: "Select what badges you would like..",
+        name: "badges",
+        choices: [
+
+        ]
+    },
+    {
+        type: "input",
+        message: "List any and all contributors..",
+        name: "contributors"
+    },
+    {
+        type: "input",
+        message: "List any tests that a user can perform..",
+        name: "tests"
+    },
+  ])
